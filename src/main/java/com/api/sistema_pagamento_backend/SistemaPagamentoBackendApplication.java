8c5911d41cec03810ac5f122da13830695eaf9e1
Daniel Penelva <d4n.andrade@gmail.com>
2025-05-07 18:53:20 -0300

    feat: criando projeto java e Spring Boot

---
+++ src/main/java/com/api/sistema_pagamento_backend/SistemaPagamentoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.sistema_pagamento_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPagamentoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPagamentoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/sistema_pagamento_backend/SistemaPagamentoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.sistema_pagamento_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagamentoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
