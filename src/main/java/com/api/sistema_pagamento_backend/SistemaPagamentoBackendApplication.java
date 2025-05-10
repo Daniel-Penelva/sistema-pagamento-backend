526c82e5cacc13a49e40c58b95a46b32200a5b79
Daniel Penelva <d4n.andrade@gmail.com>
2025-05-09 22:45:53 -0300

    feat: (funcionalidade: populando banco de dados utilizando o CommandLineRunner) add método commandLineRunner
    Neste método (1) vai ser salvo três estudantes.  (2)Vai ser gerado pagamentos aleatórios, obtendo os valores do enum do tipo de pagamento. Cria um objeto Random para gerar valores aleatórios. (3) Para cada estudante criado vai ser gerado 10 pagamentos. E para cada pagamento é associado ao estudante salvo no banco de dados, ou seja, 3 estudantes + 30 pagamentos.

--- src/main/java/com/api/sistema_pagamento_backend/SistemaPagamentoBackendApplication.java
+++ src/main/java/com/api/sistema_pagamento_backend/SistemaPagamentoBackendApplication.java
@@ -1,13 +1,73 @@
 package com.api.sistema_pagamento_backend;
 
+import java.time.LocalDate;
+import java.util.Random;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.api.sistema_pagamento_backend.entities.Estudante;
+import com.api.sistema_pagamento_backend.entities.Pagamento;
+import com.api.sistema_pagamento_backend.enums.PagamentoStatus;
+import com.api.sistema_pagamento_backend.enums.TipoPagamento;
+import com.api.sistema_pagamento_backend.repositories.EstudanteRepository;
+import com.api.sistema_pagamento_backend.repositories.PagamentoRepository;
 
 @SpringBootApplication
 public class SistemaPagamentoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPagamentoBackendApplication.class, args);
+	}
+
+	@Profile("dev") // O método só será executado se o perfil ativo for "dev"
+	@Bean           // Significa que o método retorna um objeto que deve ser gerenciado pelo Spring
+	CommandLineRunner commandLineRunner(EstudanteRepository estudanteRepository, PagamentoRepository pagamentoRepository) {
+		return args -> {
+			estudanteRepository.save(Estudante.builder()
+					.nome("Vanessa")
+					.sobrenome("Mota")
+					.codigo("4567")
+					.programaId("LTA1")
+					.build());
+
+			estudanteRepository.save(Estudante.builder()
+					.nome("Talita")
+					.sobrenome("Cunha")
+					.codigo("51234")
+					.programaId("LTA1")
+					.build());
+
+			estudanteRepository.save(Estudante.builder()
+					.nome("Rafael")
+					.sobrenome("Nunes")
+					.codigo("124990")
+					.programaId("LTA1")
+					.build());
+
+			// Gerando Pagamentos Aleatórios
+			TipoPagamento tipoPagamento[] = TipoPagamento.values();  // Pegando os valores do enum TipoPagamento
+			Random random = new Random();                            // Criando um objeto Random para gerar números aleatórios
+			
+			// Criando 10 pagamentos para cada estudante
+			estudanteRepository.findAll().forEach(estudante -> {
+				for (int i=0; i<10; i++) {
+					int index = random.nextInt(tipoPagamento.length);  // Gerando um índice aleatório para o array de tipos de pagamento
+					Pagamento pagamento = Pagamento.builder()
+							.valor(1000 + (int) (Math.random() * 20000)) // Gerando um valor aleatório entre 1000 e 21000
+							.tipoPagamento(tipoPagamento[index])
+							.pagamentoStatus(PagamentoStatus.CRIADO)
+							.data(LocalDate.now())
+							.estudante(estudante)
+							.build();						
+
+					pagamentoRepository.save(pagamento);
+				}
+			});
+		};
 	}
 
 }
